1153842a441d20227b4258377a04950e766e4f57
einokupias <31246369+einokupias@users.noreply.github.com>
2017-12-05 16:28:49 +0200

    Update init.pp

--- puppet/modules/apache/manifests/init.pp
+++ puppet/modules/apache/manifests/init.pp
@@ -1,26 +1,25 @@
-class apache {
-	
-	package {"apache2":
-		ensure => "installed",
-		allowcdrom => true,
-	}
+class apache2 {
+        package {'apache2':
+                ensure => present,
+        }
 
-	file { '/etc/apache2/mods-available/userdir.load':
-		ensure => 'link',
-		target => '/etc/apache2/mods-available/userdir.load',
-		require => Package["apache2"],
-		notify => Service["apache2"],
-	}
-	
-	file { '/etc/apache2/mods-available/userdir.conf':
-		ensure => 'link',
-		target => '/etc/apache2/mods-available/userdir.conf',
-		require => Package["apache2"],
-		notify => Service["apache2"],
-	}
+        service {'apache2':
+                ensure => "running",
+                enable => "true",
+                require => Package["apache2"],
+        }
 
-	service {'apache2':
-		ensure => running,
-		enable => true,
-	}
+        file { '/etc/apache2/mods-enabled/userdir.load':
+                ensure => 'link',
+                target => '/etc/apache2/mods-available/userdir.load',
+                notify => Service["apache2"],
+                require => Package["apache2"],
+        }
+
+        file { '/etc/apache2/mods-enabled/userdir.conf':
+                ensure => 'link',
+                target => '/etc/apache2/mods-available/userdir.conf',
+                notify => Service["apache2"],
+                require => Package["apache2"],
+        }
 }
