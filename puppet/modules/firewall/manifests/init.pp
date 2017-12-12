381835c624424eac8142b2436b9115b8f36364c3
Eino Kupias <kupias@hotmail.com>
2017-12-12 11:33:28 +0000

    added notify ufw

--- puppet/modules/firewall/manifests/init.pp
+++ puppet/modules/firewall/manifests/init.pp
@@ -7,18 +7,22 @@ class firewall {
 
 	file {'/etc/ufw/user.rules':
 		content => template ("firewall/user.rules"),
+		notify => Service["ufw"],
 	}
 	
 	file {'/etc/ufw/user6.rules':
 		content => template ("firewall/user6.rules"),
+		notify => Service["ufw"],
 	}
 
 	file {'/etc/ufw/ufw.conf':
 		content => template ("firewall/ufw.conf"),
+		notify => Service["ufw"],
+
 	}
 
 	service {'ufw':
-		ensure => running,
+		ensure => "running",
 		enable => true,
 	}
 }
